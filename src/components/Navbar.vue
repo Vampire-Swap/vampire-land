b7e02e2c9744f4594d7edd0932e0a9e09dc4d94d
Vampire Swap <vampire.swap@protonmail.com>
2021-10-21 22:56:21 +0300

    Fixed mobile navbar placement

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -16,7 +16,7 @@ const addressFormat: any = inject("addressFormat");
 <template>
   <nav class="sticky top-0 z-10">
     <div class="navbar bg-neutral text-neutral-content rounded-lg sticky top-0">
-      <div class="navbar-start">
+      <div class="navbar-start flex-1 lg:flex-initial">
           <router-link to="/" class="text-lg font-bold ml-2 text-primary flex justify-center items-center">
             <img src="../assets/Bat.png" alt="VampireSwap bat" width="62" height="62" class="mr-2">
             Vampire Land
@@ -27,7 +27,7 @@ const addressFormat: any = inject("addressFormat");
           <NavigationLink v-for="navigationLink in navigationLinks" :key="navigationLink.displayedContent" :navigationLink="navigationLink" />
       </div>
 
-      <div class="navbar-end">
+      <div class="navbar-end w-auto lg:w-1/2">
           <button @click="connect" id="wallet-button" class="btn btn-primary btn-md mr-2 normal-case text-base hidden lg:inline-flex">
             <fa-icon icon="wallet" class="mr-2" />
             {{ ACCOUNT==="Connect Wallet" ? ACCOUNT : addressFormat(ACCOUNT) }}
